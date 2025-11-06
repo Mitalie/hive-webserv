92ce4251d9722cd17f61d53bb05f36b8aebfb8b1
Anssi Mäkinen <amakinen@student.hive.fi>
2025-11-06 17:54:17 +0200

    Add Makefile rules and an example for test programs
    Separate test programs are useful for testing functionality and
    demonstrating usage of individual features before the surrounding code
    they plug into exists.

---
+++ src/testbin/example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is an example test implemented in `" __FILE__ "'.\n"
+		"Add more .cpp files in the same directory, each with their own main function.\n"
+		"Don't forget to add them to TESTS in Makefile (without the .cpp suffix).\n";
+}
