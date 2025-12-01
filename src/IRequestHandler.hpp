d3c30060ee43a8e226a7aff295ca5654a95b4a81
Anssi Mäkinen <amakinen@student.hive.fi>
2025-12-01 15:42:46 +0200

    Define generic interfaces for request handlers

---
+++ src/IRequestHandler.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstddef>
+#include <span>
+
+/*
+	Abstract interface implemented by all request handlers that allows request
+	manager to stream request body to it and report response buffer status.
+	Request handler implementations should accept a IRequestManager reference to
+	allow communicating its status back to the manager.
+*/
+class IRequestHandler
+{
+public:
+	/*
+		Release all resources of the request handler. This may be called before
+		the request handling is finished if the client connection is broken.
+	*/
+	virtual ~IRequestHandler() = 0;
+
+	/*
+		Pass incoming data from connection to the request handler.
+	*/
+	virtual void onBodyData(std::span<const char> data) = 0;
+
+	/*
+		Report the status of the response buffer to the request handler so that
+		it can pause processing if the buffer grows too large.
+	*/
+	virtual void notifyResponseBuffer(size_t bufferSize) = 0;
+};
