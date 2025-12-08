34be8ba84d65f0ff533d1b0be1f777509886857a
Anssi Mäkinen <amakinen@student.hive.fi>
2025-12-08 12:06:01 +0200

    Implement ClientHandler to bridge client sockets and request handling

---
+++ src/testbin/clientHandler.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+
+#include <fcntl.h>
+
+#include "CallbackQueue.hpp"
+#include "ClientHandler.hpp"
+#include "Poll.hpp"
+
+int main()
+{
+	int fd = open("testdata/clientHandler.txt", O_RDONLY);
+	if (fd < 0)
+		return EXIT_FAILURE;
+
+	ClientHandler s(fd);
+
+	while (true)
+	{
+		Poll::doPoll();
+		CallbackQueue::handleQueue();
+	}
+}
